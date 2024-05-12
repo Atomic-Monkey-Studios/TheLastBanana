6cf7c2c52d766141c8bf5a3113cff75c3790d3d4
Monkey Prince <kyloren3600@gmail.com>
2024-05-12 20:34:32 +0200

    +banana peel

---
+++ Assets/Scripts/BananaPeel.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BananaPeel : MonoBehaviour
+{
+
+    Collider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("BANANA PEEL");
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 400f));
+            other.transform.Rotate(0f, 0f, 90f, Space.Self);
+            // StartCoroutine(applyBananaEffect(other.gameObject.GetComponent<PlayerMovement>()));
+        }
+    }
+
+    // IEnumerator applyBananaEffect(PlayerMovement playerMovement) {
+    //     Debug.Log("aaaaaaaaaa");
+    //     float oldSpeed = playerMovement.speed;
+    //     playerMovement.speed *= 2;
+    //     yield return new WaitForSeconds(1);
+    //     playerMovement.speed = oldSpeed;
+    // }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     public float jump;
 
     public bool isJumping;
+
+    private float customTorque = 0f;
 
     // private float Move;
     private Rigidbody2D rb;
@@ -24,19 +28,42 @@ public class PlayerMovement : MonoBehaviour
         
         rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
         if (Input.GetButtonDown("Jump") && !isJumping) {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            Debug.Log(Mathf.Abs(transform.eulerAngles.z));
+            // rb.AddForce(new Vector2(rb.velocity.x, jump));
+            if (Mathf.Abs(transform.eulerAngles.z) < 0.5) rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (isJumping) {
+                customTorque += 5f;
+            } else {
+                customTorque += 20f;
+            }
+            
+            rb.AddTorque(customTorque);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (isJumping) {
+                customTorque -= 5f;
+            } else {
+                customTorque -= 20f;
+            }
+            rb.AddTorque(customTorque);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Platform")) {
             isJumping = false;
+            customTorque = 0f;
         }
     }
 
     private void OnCollisionExit2D(Collision2D other) {
         if (other.gameObject.CompareTag("Platform")) {
             isJumping = true;
+            customTorque = 0f;
+            rb.totalTorque = 0f;
         }
     }
 }
